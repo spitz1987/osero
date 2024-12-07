17f120b461d6b958456c5f74234bb9982148506f
Minato shindo <shindmina5022@outlook.jp>
2024-12-07 21:37:42 +0900

    nodemon&morgan install

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import morgan from 'morgan'
+
+
+const PORT = 3000
+
+const app = express()
+app.use(morgan('dev'))
+
+app.get('/api/hello', async(req, res) => {
+    res.json({
+        message: 'Hello Express!!'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Reversi application started: http;//localhost:${PORT}`);
+})
